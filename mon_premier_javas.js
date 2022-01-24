7a6b0a5b4afa9468274a6a2b33ffdc74260c4acb
Ophelie Sabanowski <91734657+OphelieS@users.noreply.github.com>
2022-01-24 13:35:44 +0100

    Add files via upload

---
+++ mon_premier_javas.js
@@ -0,0 +1,11 @@
+let monNom = 'ophelie';
+
+document.addEventListener('DOMContentLoaded', function(){
+    let bonjour = document.getElementById('b1');
+    bonjour.addEventListener('click', alerte);
+
+    function alerte(){
+alert('Bonjour ' +  monNom);
+                }
+                });
+
